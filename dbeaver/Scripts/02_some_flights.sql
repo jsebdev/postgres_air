7d3debc8ba6b466340e6db71c5d3b7604a901c00
Sebastian Caicedo <jsebdev@gmail.com>
2025-09-26 00:32:57 -0500

    create dbeaver project

--- dbeaver/Scripts/02_some_flights.sql
+++ dbeaver/Scripts/02_some_flights.sql
@@ -5,7 +5,21 @@ join mydatabase.postgres_air.passenger p on p.booking_id = bl.booking_id
 where f.flight_id = 1
 group by f.flight_id
 
+explain
+select f.flight_id, count(p.passenger_id)  from mydatabase.postgres_air.flight f
+join mydatabase.postgres_air.booking_leg bl on bl.flight_id  = f.flight_id
+join mydatabase.postgres_air.passenger p on p.booking_id = bl.booking_id
+where f.flight_id = 1
+group by f.flight_id
+
 -- total number of passengers by booking leg in flight id = 1
+select bl.booking_leg_id, count(p.passenger_id)  from mydatabase.postgres_air.flight f
+join mydatabase.postgres_air.booking_leg bl on bl.flight_id  = f.flight_id
+join mydatabase.postgres_air.passenger p on p.booking_id = bl.booking_id
+where f.flight_id = 1
+group by bl.booking_leg_id
+
+explain
 select bl.booking_leg_id, count(p.passenger_id)  from mydatabase.postgres_air.flight f
 join mydatabase.postgres_air.booking_leg bl on bl.flight_id  = f.flight_id
 join mydatabase.postgres_air.passenger p on p.booking_id = bl.booking_id
